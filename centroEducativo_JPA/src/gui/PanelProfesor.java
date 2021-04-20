e56e4c5e9c5ff2e18508ec060ad18188cf82af02
SabrinaOC <sabriojea@gmail.com>
2021-04-20 23:25:38 +0200

    iconos panel profesor

--- centroEducativo_JPA/src/gui/PanelProfesor.java
+++ centroEducativo_JPA/src/gui/PanelProfesor.java
@@ -7,6 +7,8 @@ import javax.swing.JToolBar;
 import java.awt.Insets;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 
@@ -35,30 +37,58 @@ public class PanelProfesor extends JPanel {
 		add(toolBar, gbc_toolBar);
 		
 		JButton btnNewButton = new JButton("");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnNewButton.setIcon(new ImageIcon(PanelProfesor.class.getResource("/gui/iconos/btnPrimer.png")));
 		toolBar.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnNewButton_1.setIcon(new ImageIcon(PanelProfesor.class.getResource("/gui/iconos/btnAnterior.png")));
 		toolBar.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnNewButton_2.setIcon(new ImageIcon(PanelProfesor.class.getResource("/gui/iconos/btnSiguiente.png")));
 		toolBar.add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnNewButton_3.setIcon(new ImageIcon(PanelProfesor.class.getResource("/gui/iconos/btnUltimo.png")));
 		toolBar.add(btnNewButton_3);
 		
 		JButton btnNewButton_4 = new JButton("");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnNewButton_4.setIcon(new ImageIcon(PanelProfesor.class.getResource("/gui/iconos/btnNuevo.png")));
 		toolBar.add(btnNewButton_4);
 		
 		JButton btnNewButton_5 = new JButton("");
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnNewButton_5.setIcon(new ImageIcon(PanelProfesor.class.getResource("/gui/iconos/btnGuardar.png")));
 		toolBar.add(btnNewButton_5);
 		
 		JButton btnNewButton_6 = new JButton("");
+		btnNewButton_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnNewButton_6.setIcon(new ImageIcon(PanelProfesor.class.getResource("/gui/iconos/btnEliminar.png")));
 		toolBar.add(btnNewButton_6);
 		
